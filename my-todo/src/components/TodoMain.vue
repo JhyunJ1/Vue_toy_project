8f7c6ba585b6b33e7c3e2a577a6495df03483625
JhyunJ1 <jhyunj1005@gmail.com>
2023-02-10 11:09:48 +0900

    create hacker

--- my-todo/src/components/TodoMain.vue
+++ my-todo/src/components/TodoMain.vue
@@ -1,7 +1,20 @@
 <template>
         
     <div class="page">
-    <header><h1>Vue Fire todo1</h1></header>
+    <header><h1>Vue Fire todo1
+        <span class="pie">
+          <svg viewBox="0 0 64 64">
+          <circle class="pie" r="32" cx="32" cy="32" style="stroke-width: 64;"></circle>
+          <circle class="slice" r="32" cx="32" cy="32" 
+          :style="{ 
+            strokeWidth: 64,
+            strokeDasharray: totalTodo + ', 201',
+            transition: 'all 0.3s linear'
+            }">
+            </circle>
+        </svg>
+        </span>
+    </h1></header>
     <main>
       <div class="todos">
         <transition name="fade">
@@ -47,29 +60,41 @@ export default {
             todos:[],
         }
     },
+    computed: {
+        totalTodo() {
+            let totalNum = 0;
+            this.todos.forEach(item => {
+                if(item.state ==='done') totalNum++;
+            });
+            return totalNum / this.todos.length * 201;
+        }
+    },
     methods: {
         addItem() {
             if (!this.addItemText) return;
             db.collection('todos').add({
                 text: this.addItemText,
-                state: 'yet'
+                state: 'yet',
+                createdAt: new Date(),
             }).then(sn => {
                 db.collection('todos').doc(sn.id).update({
                     id: sn.id
                 })
             });
-            this.todos.unshift({
-                text: this.addItemText,
-                state: 'yet'
-            })
+            // this.todos.unshift({
+            //     text: this.addItemText,
+            //     state: 'yet'
+            // })
             this.addItemText = ''
         },
         checkItem(i) {
             if (this.todos[i].state === 'yet') {
-                this.todos[i].state='done'; 
+                // this.todos[i].state='done'; 
+                db.collection('todos').doc(this.todos[i].id).update({state: 'done'});
             }
             else {
-                this.todos[i].state='yet';
+                // this.todos[i].state='yet';
+                db.collection('todos').doc(this.todos[i].id).update({state: 'yet'});
             }
         },
         editShow(i) {
@@ -84,26 +109,26 @@ export default {
                     .update({
                         text: this.editItemText
                     })
-            this.todos[this.crrEditItem].text = this.editItemText;
+            //this.todos[this.crrEditItem].text = this.editItemText;
             this.writeState = true;
             this.$refs.list.children[this.crrEditItem].className = '';
         },
         delItem(i) {
             db.collection('todos').doc(this.todos[i].id).delete()
-            this.todos.splice(i, 1);
+            // this.todos.splice(i, 1);
         }
     },
     mounted() {
         this.$refs.writeArea.focus();
-        db.collection('todos').get().then((result) => {
-            result.forEach((doc)=>{
-                console.log(doc.data())
-                this.todos.unshift(doc.data());
-    })
-});
+        // db.collection('todos').get().then((result) => {
+        //     result.forEach((doc)=>{
+        //         console.log(doc.data())
+        //         this.todos.unshift(doc.data());
+        //     })
+        // });
     },
     firestore: {
-        todos: db.collection('todos')
+        todos: db.collection('todos').orderBy('createdAt', 'desc')
     }
  
 }
